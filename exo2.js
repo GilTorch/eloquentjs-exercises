32833ba605fcd2eba3c536474e4a198af6211443
GilTorch <infotorch2014@gmail.com>
2019-09-27 08:56:07 -0400

    Finished with exercise 2 of program structures

--- exo2.js
+++ exo2.js
@@ -1,11 +1,15 @@
 for (let i = 0; i < 99; i++) {
     let num = i;
     num++;
-    if (num % 3 === 0) {
-        console.log("Fizz");
-    } else if (num % 5 === 0){
-        console.log("Buzz");
-    }else{
+    if (num % 3 === 0 && num % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (num % 3 === 0) {
+      console.log("Fizz");
+    }
+    else if (num % 5 === 0) {
+      console.log("Buzz");
+    }
+    else {
       console.log(num);
     }
-}
+  }

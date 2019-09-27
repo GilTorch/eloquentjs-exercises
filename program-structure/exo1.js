c11ee04e8b31995eeed0226997687ecfad0279f4
GilTorch <infotorch2014@gmail.com>
2019-09-27 08:39:02 -0400

    Created a folder for the exercises related to program structure and added a README

---
+++ program-structure/exo1.js
@@ -0,0 +1,10 @@
+for(let i = 0; i < 8; i++){
+    let counter = 1;
+    let character="";
+    while(counter <= i){
+      character+="#"
+      counter++;
+    }
+    console.log(character);
+    console.log("\n");
+}

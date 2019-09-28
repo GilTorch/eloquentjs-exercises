4754589bdb5fbd8fb3bd2a95e713b8eec2cb6455
GilTorch <infotorch2014@gmail.com>
2019-09-28 09:23:02 -0400

    Solved exercise 2 on functions: creating a function that tells if a number is even or odd using recursion

---
+++ functions/exo2.js
@@ -0,0 +1,10 @@
+function isEven(number){
+    number = Math.abs(number);
+    if(number === 0){
+        return true;
+    }else if(number === 1){
+        return false;
+    }else {
+        return isEven(number - 2)
+    }
+}
